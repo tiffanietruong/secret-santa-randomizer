15cfbcf4f5ac7329b50394e76acfdc5fc43ca7db
Tiffanie Truong <tiffanie.truong@mail.utoronto.ca>
2022-09-28 22:56:33 -0400

    Add routing to nav bar and landing page

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,16 +1,25 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-import NavBar from './components/NavBar/NavBar';
 import Landing from "./components/Landing/Landing";
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
-  return (
-    <div>
-      <NavBar/>
-      <Landing/>
-    </div>
-    
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route path='/' element={<Landing />} />
+                    <Route path='/randomize' element={<Landing />} />
+                    <Route path='/uni-renegades' element={<Landing />} />
+                    <Route path='/2022' element={<Landing />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+
+    );
 }
 
 export default App;
--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 import './NavBar.css';
 
 const NavBar = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="NavBar">
             <span className="right">
-                <Button variant='filled' sx={{ color: 'white', m: 1 }}>Home</Button>
-                <Button variant='filled' sx={{ color: 'white', m: 1 }}>Randomize</Button>
-                <Button variant='filled' sx={{ color: 'white', m: 1 }}>Uni Renegades</Button>
-                <Button variant='filled' sx={{ color: 'white', m: 1 }}>2022</Button>
+                <Button
+                    variant='filled'
+                    onClick={() => { navigate('/'); }}
+                    sx={{ color: 'white', m: 1 }}
+                >
+                    Home
+                </Button>
+                <Button
+                    variant='filled'
+                    onClick={() => { navigate('/randomize');  }}
+                    sx={{ color: 'white', m: 1 }}
+                >
+                    Randomize
+                </Button>
+                <Button
+                    variant='filled'
+                    onClick={() => { navigate('/uni-renegades'); }}
+                    sx={{ color: 'white', m: 1 }}
+                >
+                    Uni Renegades
+                </Button>
+                <Button
+                    variant='filled'
+                    onClick={() => { navigate('/2022'); }}
+                    sx={{ color: 'white', m: 1 }}
+                >
+                    2022
+                </Button>
             </span>
         </div>
     );
